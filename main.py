e613961c56c4bd05686e0769529a314f5feab4d4
_dzudzu_ <dtyz.dtyz2@gmail.com>
2023-09-22 14:27:45 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+import asyncio
+import os
+
+# Токен вашего бота. Замените его на свой собственный токен.
+TOKEN = 'YOUR_BOT_TOKEN_HERE'
+
+# ID канала, в который вы хотите отправить сообщение
+CHANNEL_ID = 1153046777952481420
+
+# Создание клиента Discord
+client = discord.Client()
+
+# Обработчик события "бот запущен"
+@client.event
+async def on_ready():
+    channel = client.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send("Бот запущен!")
+
+# Запуск бота
+client.run(TOKEN)
